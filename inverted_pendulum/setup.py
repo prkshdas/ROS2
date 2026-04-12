226b390a6af428215132e102f2f14a419608fb9c
Prakash <prkshhhdas@gmail.com>
2026-04-12 21:26:01 +0530

    Update: configured ROS2 to copy launch and urdf files during build

--- inverted_pendulum/setup.py
+++ inverted_pendulum/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'inverted_pendulum'
@@ -10,6 +12,10 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        
+        # configure ROS2 to copy our launch and urdf files during build
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*.urdf'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
